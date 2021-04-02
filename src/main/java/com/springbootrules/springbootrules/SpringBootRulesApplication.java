c343e9efb4681cd6bcdd46628dcdec27d6999052
abhi020290 <abhi020290@gmail.com>
2021-04-02 18:02:29 +0530

    intial commit

---
+++ src/main/java/com/springbootrules/springbootrules/SpringBootRulesApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrules.springbootrules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootrules/springbootrules/SpringBootRulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrules.springbootrules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
